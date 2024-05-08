c58ba1818fb63050c4790544253bb5b8aa051448
Arun <yarunkumar136@gmail.com>
2024-05-09 00:18:59 +0530

    create Login api with Hideen Password

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors=require("cors")
+const cors = require("cors");
 require("./db/config");
 const User = require("./db/users");
 const app = express();
@@ -8,7 +8,24 @@ app.use(express.json());
 app.post("/users", async (req, resp) => {
   const user = new User(req.body);
   const result = await user.save();
-  resp.send(result);
+  //dont show the userPassword in the response start
+  const removePassword = result.toObject();
+  delete removePassword.password;
+  //end
+  resp.send(removePassword);
 });
 
+app.post("/Login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "no user found" });
+    }
+  } else {
+    resp.send({ result: "no user found" });
+  }
+  resp.send(req.body);
+});
 app.listen(3000);
